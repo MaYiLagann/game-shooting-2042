8289ff6545f2266a54b1e42abd30796158c63ebc
MaYiLagann <lhwshy@naver.com>
2022-11-14 00:33:47 +0900

    Create script PlayerController

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Todo: Implement movement.
+    // Todo: Implement shooting.
+    // Todo: Implement use skill.
+    // Todo: Implement health and damage.
+}
