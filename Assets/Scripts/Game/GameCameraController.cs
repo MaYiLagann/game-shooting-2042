802740f7c481c898fe863951446ef84272c731f5
MaYiLagann <lhwshy@naver.com>
2022-11-13 23:07:54 +0900

    Fix wrong camera ratio adjustment on portrait resolutions

--- Assets/Scripts/Game/GameCameraController.cs
+++ Assets/Scripts/Game/GameCameraController.cs
@@ -12,17 +12,7 @@ public class GameCameraController : MonoBehaviour
     {
         // Adjust the camera orthographic size for device's screen size.
         var screenSize = new Vector2(Screen.width, Screen.height);
-        var screenRatio = screenSize.x / screenSize.y;
-        var viewRatio = ViewSize.x / ViewSize.y;
 
-        if (screenRatio >= viewRatio)
-        {
-            GameCamera.orthographicSize = ViewSize.x * screenSize.y / screenSize.x / 2f;
-        }
-        else
-        {
-            var diffSize = viewRatio / screenRatio;
-            GameCamera.orthographicSize = ViewSize.x * screenSize.y / screenSize.x / 2f * diffSize;
-        }
+        GameCamera.orthographicSize = ViewSize.x * screenSize.y / screenSize.x / 2f;
     }
 }
