db9caa9181f6d6aefd4a79252a858b263b7055e2
MaYiLagann <lhwshy@naver.com>
2022-11-19 21:47:21 +0900

    Implement player movement using character controller

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Todo: Implement movement.
     // Todo: Implement shooting.
     // Todo: Implement use skill.
     // Todo: Implement health and damage.
+
+
+
+    public CharacterController PlayerCharacterController;
+    public string InputAxisHorizontal = "Horizontal";
+    public string InputAxisVertical = "Vertical";
+
+    public float MovementSpeed = 1f;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled
+    /// </summary>
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis(InputAxisHorizontal), 0, Input.GetAxis(InputAxisVertical));
+
+        PlayerCharacterController.Move(move * Time.deltaTime * MovementSpeed);
+    }
 }
