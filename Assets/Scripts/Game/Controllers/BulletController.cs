d74796ab296346821bfeec2e64398dcca79f32b2
MaYiLagann <lhwshy@naver.com>
2022-11-20 23:58:49 +0900

    Create script BulletController

---
+++ Assets/Scripts/Game/Controllers/BulletController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public int Damage = 1;
+
+
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        var damageable = other.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            damageable.Damage(Damage);
+        }
+    }
+}
