1b7f42c2e615e2b6206ef718f9f5d7555b8654a9
MaYiLagann <lhwshy@naver.com>
2022-11-20 22:54:12 +0900

    Implement player health and damage

--- Assets/Scripts/Game/Controllers/PlayerController.cs
+++ Assets/Scripts/Game/Controllers/PlayerController.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
+using Sirenix.OdinInspector;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IDamageable
 {
     // Todo: Implement shooting.
     // Todo: Implement use skill.
-    // Todo: Implement health and damage.
 
 
 
+    [TitleGroup("Movement")]
     public CharacterController PlayerCharacterController;
     public string InputAxisHorizontal = "Horizontal";
     public string InputAxisVertical = "Vertical";
 
     public float MovementSpeed = 1f;
 
+    [TitleGroup("Animation")]
     public Animator PlayerAnimator;
     public string AnimationKeyMoveLeft = "MoveLeft";
     public string AnimationKeyMoveRight = "MoveRight";
+
+    [TitleGroup("Damageable")]
+    public int StartHealth = 100;
+    public int Health { get; set; }
+
+
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        Health = StartHealth;
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled
@@ -29,5 +46,10 @@ public class PlayerController : MonoBehaviour
 
         PlayerAnimator.SetBool(AnimationKeyMoveLeft, move.x < 0f);
         PlayerAnimator.SetBool(AnimationKeyMoveRight, move.x > 0f);
+    }
+
+    public void Damage(int damage)
+    {
+        Health -= damage;
     }
 }
