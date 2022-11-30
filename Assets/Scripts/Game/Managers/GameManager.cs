4547438df766320514bc310b34fc0c7949f3d4db
MaYiLagann <lhwshy@naver.com>
2022-12-01 00:24:16 +0900

    Add point value on GameManager

--- Assets/Scripts/Game/Managers/GameManager.cs
+++ Assets/Scripts/Game/Managers/GameManager.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int Point = 0;
+
     public float PlayerMovementSpeed = 1f;
+
+
+
+    public void AddPoint(int point)
+    {
+        Point += point;
+    }
 }
