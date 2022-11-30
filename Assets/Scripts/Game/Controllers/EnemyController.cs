464102438f6c2e7302d9b6d3960efea789040651
MaYiLagann <lhwshy@naver.com>
2022-12-01 00:23:55 +0900

    Add enemy destroy self on health under 0

--- Assets/Scripts/Game/Controllers/EnemyController.cs
+++ Assets/Scripts/Game/Controllers/EnemyController.cs
@@ -5,14 +5,28 @@ using Sirenix.OdinInspector;
 
 public class EnemyController : MonoBehaviour, IDamageable
 {
+    public GameManager GameManager;
+
     [TitleGroup("Movement")]
     public float MovementSpeed = 1f;
+
+    [TitleGroup("Loot")]
+    public int Point = 1;
 
     [TitleGroup("Damageable")]
     public int StartHealth = 1;
     public int Health { get; set; }
 
 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        Health = StartHealth;
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
@@ -26,6 +40,20 @@ public class EnemyController : MonoBehaviour, IDamageable
 
     public void Damage(int damage)
     {
-        Health -= damage;
+        if (Health > damage)
+        {
+            Health -= damage;
+        }
+        else
+        {
+            Health = 0;
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        GameManager.AddPoint(Point);
+        Destroy(gameObject);
     }
 }
