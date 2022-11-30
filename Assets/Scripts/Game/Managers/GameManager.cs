9f1db38739330a1ad38844046fd6fc31cdfda5ea
MaYiLagann <lhwshy@naver.com>
2022-12-01 00:53:34 +0900

    Add event handler OnValueChange on GameManager

--- Assets/Scripts/Game/Managers/GameManager.cs
+++ Assets/Scripts/Game/Managers/GameManager.cs
@@ -1,9 +1,12 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public float PlayerMovementSpeed = 1f;
+
+    public UnityEvent OnValueChange;
 
 
 
@@ -15,5 +18,7 @@ public class GameManager : MonoBehaviour
     public void AddPoint(int point)
     {
         Point += point;
+
+        OnValueChange.Invoke();
     }
 }
