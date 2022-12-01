cf78cc74247f7df99ff6ac1412f7ba0a74673269
MaYiLagann <lhwshy@naver.com>
2022-12-02 02:59:09 +0900

    Trying to fix game ui broken on webgl

--- Assets/Scripts/Game/Controllers/UIController.cs
+++ Assets/Scripts/Game/Controllers/UIController.cs
@@ -23,10 +23,9 @@ public class UIController : MonoBehaviour
     }
 
     /// <summary>
-    /// Start is called on the frame when a script is enabled just before
-    /// any of the Update methods is called the first time.
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
-    void Start()
+    void FixedUpdate()
     {
         UpdateUI();
     }
